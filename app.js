aa11cb20cd44488bc63a74976a715f590c44419d
Sebastianignacioqa <sebastianignacioqa@gmail.com>
2021-08-09 19:46:11 +0000

    Generando dominios .com

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-console.log("Hello world");
-
-let pronoun = ["the", "our"];
-let adj = ["great", "big"];
-let noun = ["jogger", "racoon"];
-
 let generarDominio = () => {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
   let ipronoun = Math.floor(Math.random() * pronoun.length);
   let iadj = Math.floor(Math.random() * adj.length);
   let inoun = Math.floor(Math.random() * noun.length);
 
-  return;
+  return pronoun[ipronoun]+adj[iadj]+noun[inoun]+".com";
 };
+
+
+console.log(generarDominio())
